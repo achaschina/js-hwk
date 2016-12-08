514aed2de9e10fcce561c5f84911d99afeed4549
achaschina <anna200985@gmail.com>
2016-12-08 15:24:11 +0200

    changed my .js file.
    added new function getUserNumber() and added comments

--- task7/task7.js
+++ task7/task7.js
@@ -1,28 +1,32 @@
-//To start the Game call function firstPlay() in console
-
-var prizes  = [10, 5, 2];
+var prizes = [10, 5, 2];
 
 function firstPlay() {
-    var answer = confirm("Do you want to play the game?");
+    //Ask the user is ready to start the game
+    var answer = confirm("Do you want to play the game? You have 3 attemps to guess a number");
     if (!answer) {
         console.log("Today you will not win the jackpot, but you could");
         return;
     }
-    //////////////////////////////////////////////////////////
+    //Start Game 1
     var jackPot = 0;
     jackPot = playGame(1);
+    //When user finish Game1 we offer him to play for the second time
     answer = confirm("Do you want to play the game again?");
     if (answer) {
         jackPot += playGame(3);
     }
-    console.log("Your total win: " + jackPot);
+    alert("Your total win: " + jackPot);
 }
 
 function playGame(coef) {
+    //var coef it is the coefficient for the calculation of the prize. 1 at first time and 3 at second time
     var prizesSum = 0;
     var ourNumber = Math.round(Math.random() * 5);
     for (var i = 1; i <= 3; i++) {
         var userNumber = getUserNumber();
+        if (!userNumber) {
+            break;
+        }
         if (userNumber == ourNumber) {
             prizesSum = coef * prizes[i - 1];
             alert("You won " + prizesSum + "$ in " + i + " attemp!");
@@ -33,6 +37,21 @@ function playGame(coef) {
 }
 
 function getUserNumber() {
-    var userNumber = prompt("Please enter your number.");
+    var userNumber = prompt("Please enter your number and try to guess our number");
+    if (userNumber == null) {
+        alert("Goodbay!!!");
+        return false;
+    }
+    userNumber = parseInt(userNumber);
+    while (isNaN(userNumber)) {
+        var userNumber = prompt("Please numbers only in this game!");
+        if (userNumber == null) {
+            alert("Goodbay!!!");
+            return false;
+        }
+        userNumber = parseInt(userNumber);
+    }
     return userNumber;
 }
+
+firstPlay();
