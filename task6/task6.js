5cb7113d448ef201f25e37a906e14fc2bd95d20f
achaschina <anna200985@gmail.com>
2016-12-08 15:28:04 +0200

    Update task6.js
    added new function

--- task6/task6.js
+++ task6/task6.js
@@ -1,14 +1,16 @@
 function playGame() {
     var ourNumber = Math.round(Math.random() * 100);
-    var userNumber = prompt("Please enter your number and try to guess our number");
+    var userNumber = getUserNumber();
     if (!userNumber) {
         return;
     }
     while (true) {
         if (userNumber > ourNumber) {
-            userNumber = prompt("Your number is higher than ours. Try againe!");
+            alert("Your number is higher than ours. Try againe!");
+            userNumber = getUserNumber();
         } else if (userNumber < ourNumber) {
-            userNumber = prompt("Your number is less than ours. Try againe!");
+            alert("Your number is less than ours. Try againe!");
+            userNumber = getUserNumber();
         } else {
             alert("You are right! The number is " + ourNumber);
             break;
@@ -16,4 +18,21 @@ function playGame() {
     }
 }
 
+function getUserNumber() {
+    var userNumber = prompt("Please enter your number and try to guess our number");
+    if (userNumber == null) {
+        alert("Goodbay!!!");
+        return false;
+    }
+    userNumber = parseInt(userNumber);
+    while (isNaN(userNumber)) {
+        var userNumber = prompt("Please numbers only in this game!");
+        if (userNumber == null) {
+            alert("Goodbay!!!");
+            return false;
+        }
+        userNumber = parseInt(userNumber);
+        return userNumber;
+    }
+}
 playGame();
